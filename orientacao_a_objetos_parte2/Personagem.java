e827686c7af244ab9d42d434e41142dddf650548
Gustavo Balbino <78977800+JaumVituu@users.noreply.github.com>
2024-03-14 17:52:43 -0300

    feat(ap006): implementa a classe Personagem

---
+++ orientacao_a_objetos_parte2/Personagem.java
@@ -0,0 +1,17 @@
+public class Personagem{
+    //variaveis de instancia
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    void cacar (){
+        System.out.printf("%s esta cacando...\n", nome);
+    }
+    void comer(){
+        System.out.printf("%s esta comendo...\n", nome);
+    }
+    void domrmir(){
+        System.out.printf("%s esta dormindo...\n", nome);
+    }
+}
